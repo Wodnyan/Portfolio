e5a212f510546671ed35ae5eda2f34809dd27535
Albert Gergo <gergoalbert.gta@gmail.com>
2020-12-30 14:33:34 +0200

    Top bar button styles, reworked drag hook, changed the windows grey color

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,15 +1,31 @@
 import React, { useRef } from "react";
 import useDrag from "../hooks/useDrag";
 
-const Window: React.FC = () => {
+interface Props {
+  minimizeBtn?: boolean;
+  maximizeBtn?: boolean;
+}
+
+const Window: React.FC<Props> = ({ maximizeBtn, minimizeBtn }) => {
   const dragRef = useRef(null);
-  const handleRef = useRef(null);
-  const drag = useDrag(dragRef, handleRef);
+  const { handleRef } = useDrag(dragRef);
 
   return (
     <div ref={dragRef} className="window">
-      <div draggable={false} ref={handleRef} className="top-bar"></div>
-      <i className="fas fa-lock-open"></i>
+      <div draggable={false} ref={handleRef} className="top-bar">
+        <div className="top-bar__info">
+          <p>Welcome</p>
+        </div>
+        <div className="top-bar__btn-group">
+          {minimizeBtn && (
+            <i className="fas fa-window-minimize top-bar__btn"></i>
+          )}
+          {maximizeBtn && (
+            <i className="far fa-window-maximize top-bar__btn"></i>
+          )}
+          <i className="fas fa-times top-bar__btn"></i>
+        </div>
+      </div>
     </div>
   );
 };
